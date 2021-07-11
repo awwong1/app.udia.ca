0dbc43856d0104ad55f6ed951eb6076a3c897593
Alexander Wong <alex@udia.ca>
2021-07-11 16:09:11 -0600

    fancy cube should resize

--- assets/js/scene.js
+++ assets/js/scene.js
@@ -93,26 +93,11 @@ export function beginScene() {
 
   requestAnimationFrame(render);
 
-  // const geometry = new BoxGeometry();
-  // const material = new MeshBasicMaterial({ color: 0x663399 });
-  // const cube = new Mesh(geometry, material);
-  // scene.add(cube);
-
-  // camera.position.z = 5;
-
-  // function animate() {
-  //   requestAnimationFrame(animate);
-  //   cube.rotation.x += 0.01;
-  //   cube.rotation.y += 0.01;
-  //   renderer.render(scene, camera);
-  // }
-  // animate();
-
-  // function onWindowResize() {
-  //   camera.aspect = window.innerWidth / window.innerHeight;
-  //   camera.updateProjectionMatrix();
-  //   renderer.setSize(window.innerWidth, window.innerHeight);
-  // }
-  // window.addEventListener('resize', onWindowResize, false);
+  function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+  window.addEventListener('resize', onWindowResize, false);
 
 }
